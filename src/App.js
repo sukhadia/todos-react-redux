286c575610e18bd5153c34e655481c5b0ed28be6
Neel Sukhadia <nsukhadia@callpotential.com>
2021-01-28 22:23:14 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <input />
+      <button>Add</button>
+    </div>
+  );
+}
