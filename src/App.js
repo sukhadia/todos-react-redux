790ccdd787355d95ce115c8f7f8cb0b4a734a91b
Neel Sukhadia <nsukhadia@callpotential.com>
2021-01-29 19:48:39 -0600

    Because I like to code!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
 import "./styles.css";
 
+const render = () => {
+  const todos = store.getState();
+  const todosEl = document.querySelector("#todos");
+  if (todosEl)
+    ReactDOM.render(
+      todos.map((title) => <div>{title}</div>),
+      todosEl
+    );
+};
 export default function App() {
+  const [title, setTitle] = useState("");
+  store.subscribe(render);
   return (
     <div className="App">
       <h1>Todos</h1>
-      <input />
-      <button>Add</button>
+      <input onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={(e) =>
+          store.dispatch({
+            type: "ADD_TODO",
+            title
+          })
+        }
+      >
+        Add
+      </button>
+      <div id="todos">{render()}</div>
     </div>
   );
 }
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat(action.title);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
